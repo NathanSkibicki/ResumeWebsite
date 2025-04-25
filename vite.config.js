ddae734d332eba9212c0756f2dd5f5a2b3aaf9c2
NathanSkibicki <nathans.skibicki@gmail.com>
2025-04-25 16:07:45 -0400

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    port: 3000
   }
 })
