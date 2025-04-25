b19da0f4ed31492d7130f1461bb7c2606defa968
NathanSkibicki <nathans.skibicki@gmail.com>
2025-04-25 16:31:57 -0400

    bleh

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ResumeWebsite/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
-      input: {
-        main: './index.html'
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
       }
     }
   },
